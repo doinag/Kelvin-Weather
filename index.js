ed9381a49ad6100a0690087decefab4008c23917
doinag <78366306+doinag@users.noreply.github.com>
2021-03-20 10:18:00 +0000

    Add files via upload
    Kelvin Weather Exercise

---
+++ index.js
@@ -0,0 +1,11 @@
+// Kelvin it's not changing its value in this exercise
+const Kelvin = 293;
+//Kelvin value converted to Celsius
+const Celsius = Kelvin - 273;
+// Here is the result of the subtraction
+var Celsius = 20;
+let Fahrenheit = Celsius * (9 / 5) + 32;
+// Calculating conversion 
+Fahrenheit = Math.floor(Fahrenheit);
+// This function is for rounding
+console.log(`The temperature is ${Fahrenheit} degrees Fahrenheit.`);
